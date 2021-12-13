5a12e9f5f7da0da3d41222b60089b2f93beb0c2d
Hillary-Kechem <hillarykechem92@gmail.com>
2021-12-13 11:33:14 +0300

    add function akan

---
+++ script.js
@@ -0,0 +1,28 @@
+var males= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+var females = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var daysofWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+var genders = ["Male","Female"];
+
+//Find gender values
+function getGender() {
+  var gender = document.getElementsByName("genders");
+  if (genders[0].checked == true) {
+    var gender = "male";
+  } else if (genders[1].checked == true) {
+    var gender = "female";
+  } else {
+    alert("Invalid. Must choose")
+  }
+}
+
+function getAkan() {
+  var genders = document.getElementById("genders");
+  switch(males)
+  if (daysofWeek[0]) {
+    alert("Your akan name is +" males[0])
+    }else if{daysofWeek[1]]{
+      
+    }
+
+    }
+  }
